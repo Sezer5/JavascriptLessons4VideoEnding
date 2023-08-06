68be9cecb32465918370837c75f3ac137d8e9d3f
Sezer5 <76732537+Sezer5@users.noreply.github.com>
2023-08-06 22:50:33 +0300

    first commit

---
+++ 3.Javascript Çıktı Verme/app.js
@@ -0,0 +1,19 @@
+//Document Write İle Çıktı Vermek
+document.writeln("Sezer <br>"); //Line:satır demek ln
+document.writeln("Sezer"); 
+console.log("Sezer Ünalmış 3");
+
+//Console sınıfının log metodunu kullanırız
+console.log("Hello World");
+// console.clear();
+
+let a=5;
+let b=10;
+console.log("İki Sayının toplamı=",a+b);
+
+//Alert Popup ile çıktı verme işlemi
+
+console.log(window);
+
+console.log(document.location.host);
+alert("Merhaba");
